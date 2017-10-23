a0a000e9300edcb47f9ae2632abb04447a0edc4a
Luis Nesi <luis.nesi@two-uk.com>
2017-10-23 15:43:42 +0100

    Add password encryption on save hook in mongoose

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,7 +1,12 @@
 const User = require ('../models/user');
 
+
 exports.signup = function(req,res,next){
 	const {email,password,full_name}= req.body;
+	if(!email || !password || !full_name){
+		//we can validate more on this fields.
+		return res.status(422).send({error:"You mus provided email, password and full_name"});
+	}
 	// See if the user with the given email exist 
 	User.findOne({email},(err,existingUser)=>{
 		if(err) return next(err);
